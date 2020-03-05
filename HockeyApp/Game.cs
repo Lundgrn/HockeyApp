0d89c9cf47ae1a086617b042fbfc9ecbd32838a0
Andreas Tano <andreas.tano@acecom.se>
2020-03-05 16:45:24 +0100

    Initial commit HockeyApp

---
+++ HockeyApp/Game.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace HockeyApp
+{
+    [DebuggerDisplay("{Day}, {WeekDay}, {Month}")]
+    class Game       
+    {
+        public string Month { get; set; }
+        public string WeekDay { get; set; }
+        public string Day { get; set; } 
+
+    }   
+}
--- HockeyApp/Program.cs
+++ HockeyApp/Program.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using HtmlAgilityPack;
+using System.Threading.Tasks;
+using System.Text;
 
 namespace HockeyApp
 {
@@ -6,7 +12,34 @@ namespace HockeyApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            startCrawlerasync();
+            Console.ReadLine();
+
+        }
+
+        private static async Task startCrawlerasync()   
+        {
+            var url = "https://www.difhockey.se/";
+            var httpClient = new HttpClient();
+            var html = await httpClient.GetStringAsync(url);
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(html);
+
+            var dates = new List<Game>();
+
+            var divs =
+                htmlDocument.DocumentNode.Descendants("div")
+                .Where(node => node.GetAttributeValue("class", "").Equals("rmss_c-header-games__day")).ToList();
+            foreach(var div in divs)
+            {
+                var game = new Game
+                {
+                    WeekDay = div.Descendants("span").FirstOrDefault().Descendants("span").FirstOrDefault().InnerText,
+                    Day = div.Descendants("span").FirstOrDefault().InnerText,
+                    Month = div.Descendants("span").FirstOrDefault().Descendants("span").FirstOrDefault().InnerText
+                };
+                dates.Add(game);
+            }
         }
     }
 }
