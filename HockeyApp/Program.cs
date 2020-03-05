23f546ae855c83a229013a0681389ab4803a4968
Andreas Tano <andreas.tano@acecom.se>
2020-03-05 14:08:17 +0100

    Add project files.

---
+++ HockeyApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HockeyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
